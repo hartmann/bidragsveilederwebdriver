99b53137fa1835da0df6c0fe40daa0824c9634ce
hartmann <vegard.hartmann@gmail.com>
2009-04-28 20:56:04 +0200

    skrevet test for enkel beregning

--- src/test/java/no/nav/bidragsveileder/webtest/BidragsveilederWebTest.java
+++ src/test/java/no/nav/bidragsveileder/webtest/BidragsveilederWebTest.java
@@ -1,5 +1,15 @@
 package no.nav.bidragsveileder.webtest;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import no.nav.bidragsveileder.webtest.pages.*;
+import static no.nav.bidragsveileder.webtest.pages.UnderholdskostnadPage.UtgifterBarnetilsyn.HAR_IKKE_UTGIFTER_TIL_BARNETILSYN;
+import static junit.framework.Assert.assertTrue;
+
 /**
  * User: Vegard Hartmann
  * Date: 28.apr.2009
@@ -7,20 +17,38 @@ package no.nav.bidragsveileder.webtest;
  */
 public class BidragsveilederWebTest {
 
+    private WebDriver driver;
+
+    private static final String START_PAGE = "http://tjenester.nav.no/bidragsveileder/steg1.action";
+
+    @Before
+    public void setup() {
+     //   driver = new FirefoxDriver();
+      HtmlUnitDriver htmlUnitDriver = new HtmlUnitDriver();
+      htmlUnitDriver.setJavascriptEnabled(true);
+      driver = htmlUnitDriver;
+        PageMother.setDriver(driver);
+        driver.get(START_PAGE);
+    }
+
+    @Test
     public void bidragsveilederEnkeltTilfelle() {
-        //bla igjennom letteste måte
-        //fødselsdato 01.01.2001
-        //ingen utgifter til barnetilsyn
+        UnderholdskostnadPage underholdskostnadPage = PageMother.createUnderholdskostnadPage();
+        underholdskostnadPage.registrerBarnetsFodselsdato("01.01.2001");
+        underholdskostnadPage.velgUtgifterTilBarnetilsyn(HAR_IKKE_UTGIFTER_TIL_BARNETILSYN);
+        InntektPage inntektPage = underholdskostnadPage.gaaVidere();
 
-        //personinntekt 250000
-        //skatteklasse 2
-        //ikke utvidet barnetrygd
-        //bidragspliktig inntekt 300000
-        //skatteklasse 1
-        //bor alene
+        inntektPage.registrerPersonInntektBidragsmottaker(250000);
+        inntektPage.velgSkatteklasseBidragsmottaker(InntektPage.Skatteklasse.SKATTEKLASSE_2);
+        inntektPage.velgUtvidetBarnetrygd(InntektPage.TrueFalse.FALSE);
+        inntektPage.registrerPersonInntektBidragspliktig(300000);
+        inntektPage.velgSkatteklasseBidragspliktig(InntektPage.Skatteklasse.SKATTEKLASSE_1);
+        inntektPage.velgBoforhold(InntektPage.Boforhold.ALENE);
+        SamvaerPage samvaerPage = inntektPage.gaaVidere();
 
-        //0-1 netter samvær
+        ResultatPage resultatPage = samvaerPage.gaaVidere();
 
+        assertTrue(resultatPage.isCurrentPage());
     }
 
     public void skalViseFeilmeldingDersomFodselsdatoIkkeOppgis() {
@@ -45,6 +73,11 @@ public class BidragsveilederWebTest {
     public void skalKunneEndreTidligereSteg() {
         //bla gjennom til steg4
         //gå tilbake til hvert enkelt tidligere steg
+    }
+
+    //@After
+    public void teardown() {
+        driver.close();
     }
 
 }
